f417db6d65523d1feacc7f5301316aa66d35c4ec
Danver <danver.ver@hotmail.com>
2025-01-21 18:45:46 -0300

    Blade Templates

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,43 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('admin.layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Users</title>
-</head>
+@section('title', 'Listagem dos usuários')
 
-<body>
-    <h1>Usuários</h1>
+@section('content')
 
-    <a href="{{ route('users.create') }}">Novo</a>
+<h1>Usuários</h1>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Nome</th>
-                <th>E-mail</th>
-                <th>Ações</th>
-            </tr>
-        <tbody>
-            <tr>
-                @forelse ($users as $user)
-            <tr>
-                <td>{{$user->name}}</td>
-                <td>{{$user->email}}</td>
-                <td>-</td>
-            </tr>
-            @empty
-            <tr>
-                <td colspan="100">Nenhum usuário no banco.</td>
-            </tr>
-            @endforelse
-            </tr>
-        </tbody>
-        </thead>
-    </table>
+<a href="{{ route('users.create') }}">Novo</a>
 
-    {{$users->links()}}
-</body>
+<table>
+    <thead>
+        <tr>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+        </tr>
+    <tbody>
+        <tr>
+            @forelse ($users as $user)
+        <tr>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}</td>
+            <td>-</td>
+        </tr>
+        @empty
+        <tr>
+            <td colspan="100">Nenhum usuário no banco.</td>
+        </tr>
+        @endforelse
+        </tr>
+    </tbody>
+    </thead>
+</table>
 
-</html>
+{{$users->links()}}
+
+@endsection
